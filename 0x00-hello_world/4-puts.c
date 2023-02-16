fa5fd9720a6af2cbe9c4ea985530269c108471d9
Chiemezuo <chiemezuoakujobi@gmail.com>
2023-02-16 06:33:15 -0800

    betty doc style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-/*
- *  main function to run 
- *  returns an output of 0
- *  
-*/
+/**
+ * main function to run 
+ * returns an output of 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
