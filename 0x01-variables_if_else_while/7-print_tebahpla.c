2b7534372d1d9694e686470294f821a76ae4df91
Chiemezuo <chiemezuoakujobi@gmail.com>
2023-02-17 01:10:12 -0800

    Create task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+		putchar(alphabet);
+		putchar('\n');
+
+	return (0);
+}
