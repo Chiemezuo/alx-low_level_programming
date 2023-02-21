54520f94715491324122df0806e25ebce76ab69c
Chiemezuo <chiemezuoakujobi@gmail.com>
2023-02-21 06:47:32 -0800

    task 7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0.
+ */
+int main(void)
+{
+  int r;
+
+  r = _abs(-1);
+  printf("%d\n", r);
+  r = _abs(0);
+  printf("%d\n", r);
+  r = _abs(1);
+  printf("%d\n", r);
+  r = _abs(-98);
+  printf("%d\n", r);
+  return (0);
+}
