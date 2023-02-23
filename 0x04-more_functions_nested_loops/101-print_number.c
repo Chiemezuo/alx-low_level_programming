85e8d3fad16317aa718f5b1cdec750108eaa7983
Chiemezuo <chiemezuoakujobi@gmail.com>
2023-02-23 09:12:37 -0800

    hot fix for optional task 2

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -18,19 +18,3 @@ void print_number(int n)
 	else
 		print_integer(n);
 }
-
-/**
- * print_integer - Prints n
- * @m: an unsigned integer
- * Return: Nothing
- */
-void print_integer(int m)
-{
-	int i = 1000000000;
-
-	for (; i >= 1; i /= 10)
-		if (m / i != 0)
-		{
-			_putchar((m / i) % 10 + '0');
-		}
-}
