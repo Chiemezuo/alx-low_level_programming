8d31727595b56237cdc7d97b727d0264aedcd597
Chiemezuo <chiemezuoakujobi@gmail.com>
2023-02-27 04:18:54 -0800

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * if string length is odd, n = (length_of_the_string - 1) / 2
+ * @str: input
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+
+	int a, b, longint;
+
+	longint = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+		longi++;
+
+	b = (longint / 2);
+
+
+	if ((longint % 2) == 1)
+		b = ((longint + 1) / 2);
+
+
+	for (a = b; str[a] != '\0'; a++)
+		_putchar(str[a]);
+
+	_putchar('\n');
+}
