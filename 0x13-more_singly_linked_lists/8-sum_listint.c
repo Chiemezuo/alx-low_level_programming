e112362202a1a743d4ea511e36c51415ea39bd7d
Chiemezuo <chiemezuoakujobi@gmail.com>
2023-04-03 15:00:30 +0100

    Update 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * sum_listint - A function that sums values from listint_t
- * @head: A pointer listint_t structure
- * Return: The sum of all the data n from list
+ * sum_listint - function that sums values from listint_t
+ * @head: pointer listint_t structure
+ * Return: sum of all the data n from list
  */
 int sum_listint(listint_t *head)
 {
