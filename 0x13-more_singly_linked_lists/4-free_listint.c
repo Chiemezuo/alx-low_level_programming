0ab2939bb3d33b1f99f2874082f462cdd9dee8e2
Chiemezuo <chiemezuoakujobi@gmail.com>
2023-04-03 14:52:47 +0100

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * free_listint - A function that frees a linked list
- * @head: A pointer to listint_t structure
+ * free_listint - frees a linked list
+ * @head: pointer to listint_t structure
  * Return: Nothing
  */
 void free_listint(listint_t *head)
