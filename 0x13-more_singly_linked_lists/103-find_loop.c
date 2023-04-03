e31f6f624b678bca9d00dc8d830f8d18e73652cb
Chiemezuo <chiemezuoakujobi@gmail.com>
2023-04-03 18:42:24 +0100

    Update 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,22 +1,39 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - function that finds the loop in a list
- * @head: pointer to listint_t structure
- * Return: address of the node where the loop start, or NULL
+ * find_listint_loop - Finds loop contained in
+ * a listint_t linked list.
+ * @head: pointer to the head of the listint_t list.
+ * Return: NULL if no loop, else address of node where loop starts.
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *one = head, *two = head;
+	listint_t *placeholder, *place2;
 
-	while (one && two && two->next)
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	placeholder = head->next;
+	place2 = (head->next)->next;
+
+	while (place2)
 	{
-		one = one->next;
-		two = two->next->next;
-		if (one == two)
+		if (placeholder == place2)
 		{
-			return (one);
+			placeholder = head;
+
+			while (placeholder != place2)
+			{
+				placeholder = placeholder->next;
+				place2 = place2->next;
+			}
+
+			return (placeholder);
 		}
+
+		placeholder = placeholder->next;
+		hare = (hare->next)->next;
 	}
+
 	return (NULL);
 }
