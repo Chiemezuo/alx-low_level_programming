3ab579d04aa1966d91dca65b9182bbec7b66bf51
Chiemezuo <chiemezuoakujobi@gmail.com>
2023-04-03 15:10:32 +0100

    Update 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,14 +9,14 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *new_node;
-	int n;
+	int endpoint;
 
 	if (*head == NULL)
 		return (0);
 
-	n = (*head)->n;
+	endpoint = (*head)->n;
 	new_node = (*head)->next;
 	free(*head);
 	*head = new_node;
-	return (n);
+	return (endpoint);
 }
