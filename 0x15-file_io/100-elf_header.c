f06b3c32a49516002e13a1701159fcf04aac132f
Chiemezuo <chiemezuoakujobi@gmail.com>
2023-04-06 21:59:59 +0100

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - The entry point for program to get header of ELF file
+ * main - Entry point for program to get header of ELF file
  * @argc: The number of arguments
  * @argv: The pointer to array of arguments
- * Return: 1 on success, error code on failure
+ * Return: 1
  */
 int main(int argc, char *argv[])
 {
